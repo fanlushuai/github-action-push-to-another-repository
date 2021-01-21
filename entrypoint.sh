a7dd92792966e99ebb2606bd7db1562d2c99ff38
auh <21078607+fanlushuai@users.noreply.github.com>
2021-01-21 08:43:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,6 +45,8 @@ mv "$CLONE_DIR" "$TARGET_DIR"
 echo "Copying contents to git repo"
 cp -rf "$SOURCE_DIRECTORY"/. "$TARGET_DIR"
 
+mv "$CLONE_DIR/.git" "$TARGET_DIR"
+
 ;;
 'target repo priority')
 mv "$CLONE_DIR" "$TARGET_DIR"
